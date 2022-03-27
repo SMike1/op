d88b513a363f1befcc777f01163b194035a36be1
SMike1 <101955222+SMike1@users.noreply.github.com>
2022-03-27 13:07:43 +0300

    Add files via upload

---
+++ task_A288.py
@@ -0,0 +1,26 @@
+# Задан список положительных чисел arr и положительное число newavg.
+# Написать функцию new_avg(arr, newavg), которая возвращает наименьшее целое число,
+# которое нужно добавить к списку, чтобы среднее значение было больше или равно newavg
+#
+# Пример:
+# new_avg([14, 30, 5, 7, 9, 11, 15], 30) ==> 149
+
+
+import traceback
+
+
+def new_avg(arr, newavg):
+    # Тело функции
+    return 0
+
+
+# Тесты
+try:
+    assert new_avg([14, 30, 5, 7, 9, 11, 15], 30) == 149
+    assert new_avg([14, 30, 5, 7, 9, 11, 16], 90) == 628
+    assert new_avg([14, 30, 5, 7, 9, 11, 15], 92) == 645
+except AssertionError:
+    print("TEST ERROR")
+    traceback.print_exc()
+else:
+    print("TEST PASSED")
