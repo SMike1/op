08036513cac626cf55a17abc11c7943ce705f773
SMike1 <101955222+SMike1@users.noreply.github.com>
2022-05-04 22:05:09 +0300

    Update DMath2.py

--- DMath2.py
+++ DMath2.py
@@ -1,15 +1,15 @@
 import math
 
 
-'''
+
 def perVirag(k, b,z,x ):
     d = (1-0.5*k)/b*math.sqrt(math.fabs(z))
     q = math.log(k)*x
     r = 0.025*math.log(b, 5)*math.pow(k,3)
     a = q + d - r
     return a
-print((perVirag(1, 0.5, 1, 0)))
-'''
+
+
 
 def cVirag(z, k, x):
     q = (math.pow(math.sin(z),2) + math.cos(math.pow(k,2)) * x) * 8
@@ -17,5 +17,6 @@ def cVirag(z, k, x):
     c = q + r
     return c
 print(cVirag(0, 0, 2))
+print((perVirag(1, 0.5, 1, 0)))
 
 
